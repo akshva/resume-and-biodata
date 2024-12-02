7965479ab239b7f8e6f46a3004471233a171dcc0
akshva <158309531+akshva@users.noreply.github.com>
2024-12-02 12:59:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// // Dark Mode Toggle
+// const themeToggle = document.getElementById('theme-toggle');
+
+// // Check localStorage for the theme preference
+// const savedTheme = localStorage.getItem('theme');
+// if (savedTheme === 'dark') {
+//   document.body.classList.add('dark-mode');
+//   themeToggle.textContent = 'Light Mode';
+// }
+
+// // Toggle dark mode and save preference
+// themeToggle.addEventListener('click', () => {
+//   const isDarkMode = document.body.classList.toggle('dark-mode');
+//   themeToggle.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
+//   localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+// });
